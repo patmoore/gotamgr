84bba89fe3d133777effbd5aaf77e78cff461b2f
Patrick Moore <patmoore@farreach.es>
2014-06-27 08:15:16 -0700

    run-node-inspector.sh

---
+++ run-node-inspector.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Port to host the inspector
+#    Host to listen on
+#    --web-host={String} (default: "")
+#node-inspector --web-port=9003 --debug-port=7002
+node-inspector --web-port=9003
+
+
