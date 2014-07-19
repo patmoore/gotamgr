df7b9816e0e84861fc89692b25f94f08ee72f87a
Patrick Moore <patmoore@farreach.es>
2014-07-19 00:14:08 -0700

    globalhelper.js

--- client/views/globalhelpers.js
+++ client/views/globalhelpers.js
@@ -1,10 +1,44 @@
+/**
+ * When you determine that a helper is universal. Move the definition into this array to have the function get registered
+ * automatically.
+ *
+ * Global helpers are overridden by a templates
+ */
+_.each({
+    isSelected: function(option, arr) {
+        if (arr) {
+            if (typeof(arr) === 'string' || typeof(arr) === 'number') {
+                if (option == arr) {
+                    return { selected : "selected"};
+                }
+            } else {
+                for (var i in arr) {
+                    if (option == arr[i]) {
+                        return { selected : "selected"};
+                    }
+                }
+            }
+        }
+        return null;
+    },
+
+    radioChecked: function(value, target, isDefault) {
+        if ((!target && isDefault === true) || target == value) {
+            return { checked: 'checked'};
+        }
+        return null;
+    }
+
+}, function(func, helperKey) {
+    UI.registerHelper(helperKey, func);
+});
 // HACK PATM 16 may 2014: Sadly UI.body.events() does not work to add these event handlers. :-(
 DefaultEventHandlers = {
-/**
- * Note: That data-route-data must use " around the json keys and elements ' will not work
- * <button data-route='route_name' data-route-data='{"customerProjectId":"{{id}}"}'>
- * @type {{click button[data-route]: 'click button[data-route]'}}
- */
+    /**
+     * Note: That data-route-data must use " around the json keys and elements ' will not work
+     * <button data-route='route_name' data-route-data='{"customerProjectId":"{{id}}"}'>
+     * @type {{click button[data-route]: 'click button[data-route]'}}
+     */
     'click button[data-route]' : function(event) {
         var route = $(event.currentTarget).data('route');
         // automatically converts to JSON if possible.
@@ -17,8 +51,8 @@ DefaultEventHandlers = {
         Router.go(route, routeData);
     },
     /**
-     * watch all elements labeled as input fields (they have a data-whalepath)
-     * use getWhalePathData() on a saveChanges handler.
+     * watch all elements labeled as input fields (they have a data-inputfield)
+     * use getChangedInputFieldData() on a saveChanges handler.
      * @param event
      */
     'change [data-inputfield]' : function(event) {
@@ -30,12 +64,13 @@ DefaultEventHandlers = {
 };
 
 /**
+ * get the values of fields that have a data-inputfield that have changed
  * TODO: would like to not make this so global.
  * @param template
  */
 getChangedInputFieldData = function(template) {
     var changeMap = {};
-    template.$('.userChanged[data-inputfield]').each(function(index, element) {
+    ($ || template.$)('.userChanged[data-inputfield]').each(function(index, element) {
         var $element = $(element);
         var key = $element.data('inputfield');
         if ( !key ) {
@@ -46,6 +81,38 @@ getChangedInputFieldData = function(template) {
     return changeMap;
 }
 
+getInputFieldData = function(template) {
+    var changeMap = {};
+    ($ || template.$)('[data-inputfield]').each(function(index, element) {
+        var $element = $(element);
+        var key = $element.data('inputfield');
+        if ( !key ) {
+            key = $element.attr('name');
+        }
+        changeMap[key] = $element.val();
+    });
+    return changeMap;
+}
+
+
+/**
+ * TODO: would like to not make this so global.
+ * @param template
+ */
+setInputFieldData = function(template, source) {
+    var changeMap = {};
+    ($ || template.$)('[data-inputfield]').each(function(index, element) {
+        var $element = $(element);
+        var key = $element.data('inputfield');
+        if ( !key ) {
+            key = $element.attr('name');
+        }
+        if (_.has(source, key)) {
+            $element.val(source[key]);
+        }
+    });
+    return changeMap;
+}
 /**
  * TODO: would like to not make this so global.
  * On successful save clear the userChanged class
@@ -54,7 +121,7 @@ getChangedInputFieldData = function(template) {
  */
 clearChangedInputFieldData = function(template, error, data) {
     if ( !error ) {
-        template.$('.userChanged[data-inputfield]').each(function(index, element) {
+        ($ || template.$)('.userChanged[data-inputfield]').each(function(index, element) {
             var $element = $(element);
             $element.removeClass('userChanged');
         });
