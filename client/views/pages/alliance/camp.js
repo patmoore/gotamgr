353f84a0bb158bf1b74557458d2b48a7a4b2fe1a
Patrick Moore <patmoore@farreach.es>
2014-09-03 22:51:27 -0700

    fix camp template

--- client/views/pages/alliance/camp.js
+++ client/views/pages/alliance/camp.js
@@ -2,13 +2,29 @@ var allianceCampsHandle;
 var allianceCampsDeps = new Deps.Dependency;
 
 Template.alliance_camp.helpers({
-    waitOn:
-    availableCampLocations: function() {
-        allianceCampsDeps.depends();
+    setup: function() {
         var params = getRouterParams();
-        var availableLocationsForAlliance = CampManager.availableLocationsForAlliance(params.allianceId);
-        debugger;
-        return availableLocationsForAlliance;
+        var availableLocationsForAllianceHandle = CampManager.availableLocationsForAllianceHandle(params.allianceId);
+        var allianceCampsHandle = CampManager.allianceCampsHandle(params.allianceId);
+        if ( allianceCamp)
+        return {
+            availableLocationsForAlliance : availableLocationsForAllianceHandle,
+            allianceCamps : allianceCampsHandle,
+            allianceId : params.allianceId
+        };
+    },
+    waitOn: function() {
+        var setup = Template.alliance_camp.setup();
+        var waitOnValues = _.where(setup); // object has a ready() function
+    },
+    data: function() {
+        var setup = Template.alliance_camp.setup();
+        var dataMap = {};
+        _.each(setup, function(element, key){
+            if ( setup[key]) {
+                dataMap[key] = setup[key].findFetch();
+            }
+        });
     },
     allianceOfficer: function() {
         var player = PlayerManager.findOneCurrentPlayer();
@@ -17,13 +33,13 @@ Template.alliance_camp.helpers({
     allianceLeader: function() {
         var player = PlayerManager.findOneCurrentPlayer();
         return player && player.allianceOfficer;
-    },
-    selectedCamp:
+    }
 });
 
 Template.alliance_camp.events({
    'click .buildCamp': function(event, template) {
-       AllianceManager.buildCamp();
+       var jsonInput = getInputFieldData();
+       AllianceManager.buildCamp(allianceId, jsonInput);
    }
 });
 
