3285748aabeeb56bf6d8810a8e2d4b6890e8a16b
Patrick Moore <patmoore@farreach.es>
2014-09-10 12:28:37 -0700

    player layout

--- client/views/pages/alliance/players.js
+++ client/views/pages/alliance/players.js
@@ -1,36 +1,21 @@
 Template.alliance_players.helpers({
-    waitOn: function() {
-        var handles = [];
-        var allianceId = getRouterParams().allianceId;
-        handles.push(AllianceManager.selectedAllianceHandle(allianceId));
-        return handles;
-    },
-    data: function() {
-        var allianceId = getRouterParams().allianceId;
+    initializeData: function(params) {
+        var allianceId = params.allianceId;
+
         return {
-            selectedAlliance: AllianceManager.findOneSelectedAlliance(allinaceId)
-        };
-    },
-    selectedAlliance: function() {
-        var allianceId = Router.current(true).params.allianceId;
-        var handle = AllianceManager.selectedAllianceHandle(allianceId);
-        if ( handle.ready() ) {
-            return handle.findOne();
-        } else {
-            return void(0);
-        }
-    },
-    players : function() {
-        var allianceId = Router.current(true).params.allianceId;
-        var handle = AllianceManager.alliancePlayersHandle(allianceId);
-        if ( handle.ready()) {
-            var results = handle.findFetch();
-            return results;
-        } else {
-            return void(0);
+            allianceId : allianceId,
+            selectedAlliance: one(AllianceManager.selectedAllianceHandle(allianceId)),
+            players: AllianceManager.alliancePlayersHandle(allianceId)
         }
     },
     AllianceRole: function() {
-        return AllianceRole;
+        return AllianceRole.symbols();
     }
 });
+
+Template.alliance_players.events({
+    'click .saveChanges': function(event, template) {
+        var json = getInputFieldData(template);
+
+    }
+})
